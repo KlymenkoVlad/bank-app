200ce9b12ac88d6fc721b377e4f903a8cd5edc89
Vlad Klymenko <klymenvlad@gmail.com>
2024-07-19 17:17:15 +0200

    project packages update

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,10 +1,15 @@
+import { useInView } from 'react-intersection-observer';
 import { features } from '../constants';
 import styles, { layout } from '../style';
 import Button from './Button';
+import '../index.css';
 
 function FeatureCard({ icon, title, content, index }) {
+  const { ref, inView } = useInView();
+  console.log(inView);
+
   return (
-    <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? 'mb-6' : 'mb-0'} feature-card cursor-pointer`}>
+    <div ref={ref} className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? 'mb-6' : 'mb-0'} ${inView ? 'animate__fadeIn' : 'opacity-0'}  feature-card cursor-pointer`}>
       <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
         <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain" />
       </div>
